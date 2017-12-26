d458e016c901e7f8273fb8364f3b380b78315dc4
Luis Fernando <xvleg3ndaryxv@gmail.com>
2017-12-26 13:49:59 -0600

    Initial Commit

---
+++ WannaFind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WannaFind
+//
+//  Created by Luis Santiago  on 12/26/17.
+//  Copyright © 2017 Luis Santiago . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
